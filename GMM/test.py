d8106996231b9ebcae58bb63875cc2693831181f
wz0919 <61940467+wz0919@users.noreply.github.com>
2020-12-04 01:26:22 +0800

    add gmm

---
+++ GMM/test.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D #This is for 3d scatter plots.
+import math
+import random
+import functools
+from gmm import *
+
+X = np.load("data.npy")[:,:3]
+
+iterations = 2000
+K = 4
+mu, sigma, pi = EM(X, K, iterations)
+
+def allocator(pi, mu, sigma, X, k):
+    N = X.shape[0]
+    cluster = []
+    for ix in range(N):
+        prospective_k = classify(pi, mu, sigma, X[ix, :])
+        if prospective_k == k:
+            cluster.append(X[ix, :])
+    return np.asarray(cluster)
+
+colours = ['r', 'g', 'b', 'y']
+fig = plt.figure(figsize=(16, 10))
+ax = fig.add_subplot(111, projection='3d')
+for k in range(K):
+    cluster = allocator(pi, mu, sigma, X[:, :3], k)
+    ax.scatter(cluster[:,0], cluster[:,1], cluster[:, 2], c=colours[k])
+plt.savefig('result.png')
+
